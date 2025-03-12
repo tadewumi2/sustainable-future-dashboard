72b7a99330f3ae85364a2d2a810fbe9bddabbf65
Tosin Adewumi <41330856+tadewumi2@users.noreply.github.com>
2025-03-12 08:02:10 +0000

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to the Sustainable Future Dashboard!</h1>
+      <p>Let’s build something awesome 🚀</p>
+    </div>
+  );
+}
